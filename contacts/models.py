326ff4c2aea389d78203b697f754ff2171501b26
Matt Miller <mmiller@hisg.org>
2009-08-18 16:59:24 -0500

    Fixed error in CachedContacts model

--- contacts/models.py
+++ contacts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 class CachedContacts(models.Model):
-   cc_id = models.IntegerField(primary_key=True)
    user = models.ForeignKey(User)
    contact_id = models.IntegerField()
    contact_name = models.CharField(max_length=32)
